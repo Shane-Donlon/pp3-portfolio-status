532bf7286772fca15b90c8fb54ca5a3837b2a321
Shane-Donlon <shane.donlon@live.com>
2023-08-11 13:21:06 +0100

    Inital imports for Sheets Access, API functioning  and Pythonping tested as successful

--- run.py
+++ run.py
@@ -1,3 +1,27 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.oauth2.service_account import Credentials
+from pythonping import ping
+import socket
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+]
+
+# const
+CREDS = Credentials.from_service_account_file("creds.json")
+# const
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+# const
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+# const
+SHEET = GSPREAD_CLIENT.open("portfolio-status")
+
+main_sheet = SHEET.worksheet("portfolio")
+sites_sheet = SHEET.worksheet("sites")
+
+sites_sheet_data = sites_sheet.col_values(1)[1:]  # ignores heading in A1 in Google Sheet
+for site in sites_sheet_data:
+   a =  ping(site)
+   print(a)
